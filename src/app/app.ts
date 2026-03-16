4139d6afa246ebbfd897a5cca7cb8c93119cc04f
Sumanta Rakshit <sumantarakshit6299@gmail.com>
2026-03-16 20:52:01 +0530

    custom component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Login } from './login/login';
+import { SigninComponent } from './signin/signin';
 
 @Component({
   selector: 'app-root',
-  imports:[Login],
+  imports:[Login,SigninComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
