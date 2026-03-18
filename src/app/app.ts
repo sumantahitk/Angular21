f26a45c36f841ac07938623d1c6827586ac33605
Sumanta Rakshit <sumantarakshit6299@gmail.com>
2026-03-18 21:20:01 +0530

    computed-signal

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 interface User{
@@ -14,36 +14,25 @@ interface User{
 })
 export class App {
  count= signal<number>(10);//signal created
- val=20
- name= signal<string>('Angular')
-
- num=signal<number[]>([1,2,3,4]);
  
- user=signal<{name:string,age:number}>({
-  name:'Sumanta',
-  age:23
- })
+ doubleCount = computed<number>(()=>this.count()*2)
 
- u=signal<User>({
-   name:'Sumanta',
-  age:23
- })
- // count.update(c=>c+1);
-    increment(){
-      this.count.update(c=>c+1);
-    }
-    decrement(){
-      this.count.update(c=>c-1);
-    }
-    reset(){
-      this.count.set(0);
-    }
+ tripleCount=computed<number>(()=>this.count()*3)
 
-    addNumber(){
-    this.num.update(arr=>[...arr,6])
-}
+ increment(){
+  this.count.set(this.count()+1);
+ }
 
-setNumber(){
-  this.num.set([100,200,300])
-}
+firstName = signal<string>('Praveen');
+lastName = signal<string>('Kumar');
+
+fullName = computed<string>(() => 
+  `${this.firstName()} ${this.lastName()}`
+);
+
+price=signal<number[]>([10,23,45,27,13])
+total=computed<number>(()=>{
+  return this.price().reduce((acc,curr)=> acc+curr,0);
+})
+
 }
