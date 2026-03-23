eeaab72de83a396a6ec1549f76e2a419ca1c66ef
Sumanta Rakshit <sumantarakshit6299@gmail.com>
2026-03-23 10:48:14 +0530

    card component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,42 +1,18 @@
-import { CommonModule } from '@angular/common';
+
 import { Component, signal } from '@angular/core';
-import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { email } from '@angular/forms/signals';
+import { Card } from './card/card';
+
 
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule,ReactiveFormsModule,CommonModule],
+  imports: [Card],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
 
-  // name =signal('');
-  // email=signal('')
-
-  // submitform(){
-  //   console.log(`Name: ${this.name()} , Email: ${this.email()}`);
-  // }
-
-  userSignal=signal({name:'',email:''});
-
-  form:any;
-
-  constructor(private fb:FormBuilder){
-    this.form=this.fb.group({
-      name:[''],
-      email:['']
-    })
-
-    this.form.valueChanges.subscribe((value:any)=>{
-      this.userSignal.set(value);
-    })
-
-  }
-    submitForm(){
-    console.log(`Name: ${this.userSignal().name} , Email: ${this.userSignal().email}`);
-  }
+  
   
   
 }
