752439dbbdb9896af0617deec626089cfbcd8ebe
Sumanta Rakshit <sumantarakshit6299@gmail.com>
2026-03-24 23:08:58 +0530

    working on page not found component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,16 @@ import { Routes } from '@angular/router';
 import { HomeComp } from './home-comp/home-comp';
 import { AboutComp } from './about-comp/about-comp';
 import { ContactComp } from './contact-comp/contact-comp';
+import { PageNotFound } from './page-not-found/page-not-found';
 
 
 export const routes: Routes = [
    
  {path:'',component:HomeComp},
  {path:'about',component:AboutComp},
- {path:'contact', component:ContactComp}
+ {path:'contact', component:ContactComp},
+
+
+ //WildCard route for a 404 page can be added here
+ {path:'**' ,component:PageNotFound,}
 ];
