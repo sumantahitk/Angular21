68b5ec6a40c40e31c70cd08bbb38bd1f41dd88dc
Sumanta Rakshit <sumantarakshit6299@gmail.com>
2026-03-25 02:08:01 +0530

    canDeactive Guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,18 @@ import { Routes } from '@angular/router';
 import { Login } from './login/login';
 import { Dashboard } from './dashboard/dashboard';
 import { authGuard } from './auth-guard';
+import { Profile } from './profile/profile';
+import { canDeactivateGuard } from './can-deactivate-guard';
 
 
 export const routes: Routes = [
    
     {path:'login' , component:Login},
     {path:'dashboard', component:Dashboard, canActivate:[authGuard]},
+    {
+        path:'profile',
+        component:Profile,
+        canDeactivate:[canDeactivateGuard]
+    },
     {path:'',redirectTo:'login',pathMatch:'full'}
 ];
---
+++ src/app/profile/profile.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-profile',
+  imports: [RouterLink],
+  templateUrl: './profile.html',
+  styleUrl: './profile.css',
+})
+export class Profile {
+  canDeactivate(){
+    return confirm ("Do you want to leave this page ?");
+  }
+}
