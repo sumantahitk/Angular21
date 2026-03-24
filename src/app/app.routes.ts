e3a332b2fba93e1f35406d9c5460c9c6f141c638
Sumanta Rakshit <sumantarakshit6299@gmail.com>
2026-03-24 19:14:22 +0530

    nested routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,23 @@ import { HomoComp } from './homo-comp/homo-comp';
 import { AboutComp } from './about-comp/about-comp';
 import { ContactComp } from './contact-comp/contact-comp';
 
+import { Dashboard } from './dashboard/dashboard';
+import { Profile } from './dashboard/profile/profile';
+import { Setting } from './dashboard/setting/setting';
+
 export const routes: Routes = [
     {path:'',component:HomoComp},
     {path:'about',component:AboutComp},
-    {path:'contact',component:ContactComp}
+    {path:'contact',component:ContactComp},
+
+    {path:'dashboard',component:Dashboard,
+        children:[
+            {path:'profile',component:Profile},
+            {path:'setting',component:Setting}
+        ]
+    },
+
+    {path:'',redirectTo:'',pathMatch:'full'}
+
 
 ];
---
+++ src/app/dashboard/dashboard.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [RouterLink,RouterOutlet],
+  templateUrl: './dashboard.html',
+  styleUrl: './dashboard.css',
+})
+export class Dashboard {}
