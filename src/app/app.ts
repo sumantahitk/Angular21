9e81cde9432afce3ee18a5bd2f5b77eb0a164677
Sumanta Rakshit <sumantarakshit6299@gmail.com>
2026-03-28 16:52:05 +0530

    bootstrap'

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,6 @@ import { CommonModule } from '@angular/common';
 import { Component, inject, signal, Signal } from '@angular/core';
 
 
-import { toSignal } from '@angular/core/rxjs-interop';
-import { User } from './models/user';
-import { UserService } from './services/user';
-
 
 
 @Component({
@@ -17,119 +13,11 @@ import { UserService } from './services/user';
 })
 export class App {
 
-  users = signal<User[]>([]);
-
-  name = signal<string>('');
-  email = signal<string>('');
-  editingUpdateId = signal<number | null>(null);
+ name=signal('');
 
 
-  // Clicked Edit
-  editUser(user: User) {
-    this.editingUpdateId.set(user.id!);
-    this.name.set(user.name);
-    this.email.set(user.email);
-
+  // Submit function
+  submit() {
+    console.log('User Name:', this.name());
   }
-
-  constructor(private userService: UserService) { }
-
-  // ngOnInit(){
-  //   this.userService.getUsers().subscribe((data:any)=>{
-  //     this.users=data;
-  //   })
-  // }
-
-  ngOnInit() {
-    this.loadUsers();
-  }
-
-  loadUsers() {
-    this.userService.getUsers().subscribe(data => { this.users.set(data) }
-    )
-  }
-
-  //Add and Update User
-
-  // submitForm() {
-  //   const payload: User = {
-  //     name: this.name(),
-  //     email: this.email(),
-  //     isActive: false
-  //   };
-
-  //   //Update User
-  //   if (this.editingUpdateId() != null) {
-  //     this.userService.updateUser(
-  //       this.editingUpdateId()!,
-  //       payload
-  //     ).subscribe(() => {
-  //       alert('User Update Successfully');
-  //       this.afterSave();
-  //     })
-  //   }
-  //   else{
-  //     this.userService.addUser(payload).subscribe(() => {
-  //     alert('User Added Successfully');
-  //     this.afterSave();
-  //   })
-  //   }
-    
-  // }
-
-  submitForm() {
-  const payload: User = {
-    name: this.name(),
-    email: this.email(),
-    isActive: false
-  };
-
-  // ✅ Update
-  if (this.editingUpdateId() != null) {
-    this.userService.updateUser(
-      this.editingUpdateId()!,
-      payload
-    ).subscribe(() => {
-      alert('User Updated Successfully');
-      this.afterSave();
-    });
-
-  } 
-  // ✅ Add
-  else {
-    this.userService.addUser(payload).subscribe(() => {
-      alert('User Added Successfully');
-      this.afterSave();
-    });
-  }
-}
-
-  afterSave() {
-    this.loadUsers();
-    this.name.set('');
-    this.email.set('');
-    this.editingUpdateId.set(null);
-  }
-
-  deleteUser(user:User){
-    const confirmDelete= confirm(`Are you sure you want to delete ${user.name}`);
-    if(!confirmDelete) return;
-
-    this.userService.deleteUser(user.id!).subscribe(()=>{
-      this.users.update(list=>
-        list.filter(u=> u.id !==user.id)
-      )
-    })
-
-  }
-  toggleStatus(user:User){
-    this.userService.updateUserStatus(user.id!,!user.isActive).subscribe(()=>{
-      this.users.update(list=>
-        list.map(u=> 
-          u.id===user.id ?{...u, isActive:!u.isActive}:u
-        )
-      )
-    })
-  }
-
 }
